bd11733b50f2e72c810899c9a0a12a823cd83733
Bogdan Dovgopol <bogdandovgopol@macbook.local>
2019-10-05 15:33:08 +1000

    empty project

---
+++ iOS/Urgence/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Urgence
+//
+//  Created by Bogdan Dovgopol on 5/10/19.
+//  Copyright © 2019 Urgence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
