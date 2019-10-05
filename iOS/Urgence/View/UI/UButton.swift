76f12fb11f178fdfeed03882b1f4c606b021e1f4
Bogdan Dovgopol <bogdandovgopol@macbook.local>
2019-10-05 19:08:09 +1000

    Added custom classes for custom UIButton, UITextView

---
+++ iOS/Urgence/View/UI/UButton.swift
@@ -0,0 +1,31 @@
+//
+//  UButton.swift
+//  Urgence
+//
+//  Created by Bogdan Dovgopol on 5/10/19.
+//  Copyright © 2019 Urgence. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class UButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setup()
+    }
+    
+    func setup() {
+        //update button view
+    }
+}
