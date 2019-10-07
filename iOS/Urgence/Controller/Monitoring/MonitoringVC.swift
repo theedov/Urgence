8f705e89f5577e0c9dc388ac2eae5fddd9dbed4c
Bogdan Dovgopol <bogdandovgopol@macbook.local>
2019-10-07 15:39:31 +1100

    presentSignInVC func created

--- iOS/Urgence/Controller/Monitoring/MonitoringVC.swift
+++ iOS/Urgence/Controller/Monitoring/MonitoringVC.swift
@@ -14,17 +14,14 @@ class MonitoringVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        presentSignInVC()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func presentSignInVC(){
+        let storyboard = UIStoryboard(name: Storyboard.AuthStoryboard, bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: StoryboardId.Auth)
+        present(controller, animated: false, completion: nil)
     }
-    */
 
 }
