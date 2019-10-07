c826b5f9be7d3d332138008eb415c4bdcfe073ef
Bogdan Dovgopol <bogdandovgopol@macbook.local>
2019-10-07 18:13:05 +1100

    Implemented Firebase signIn

--- iOS/Urgence/Controller/Auth/SignInVC.swift
+++ iOS/Urgence/Controller/Auth/SignInVC.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
-
+    //Outlets
+    @IBOutlet weak var emailTxt: UTextField!
+    @IBOutlet weak var passwordTxt: UTextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func enterHouseClicked(_ sender: Any) {
+        //check if singin details are empty
+        guard let email = emailTxt.text, email.isNotEmpty,
+            let password = passwordTxt.text, password.isNotEmpty else {
+                debugPrint("Email or password cannot be empty")
+                return
+        }
+        
+        activityIndicator.startAnimating()
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] result, error in
+            //check if there is an error
+            if let error = error {
+                debugPrint(error.localizedDescription)
+                self?.activityIndicator.stopAnimating()
+                return
+            }
+            
+            self?.activityIndicator.stopAnimating()
+            self?.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    
 }
 
