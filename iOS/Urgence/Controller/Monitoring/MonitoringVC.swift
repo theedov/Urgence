f887f895ef991bee1da834f11105927691bc5a77
Bogdan Dovgopol <bogdandovgopol@macbook.local>
2019-10-10 01:11:03 +1100

    logout button

--- iOS/Urgence/Controller/Monitoring/MonitoringVC.swift
+++ iOS/Urgence/Controller/Monitoring/MonitoringVC.swift
@@ -7,33 +7,47 @@
 //
 
 import UIKit
+import Firebase
 
 class MonitoringVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tap)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        isSignedIn()
+    }
+    
+    fileprivate func isSignedIn() {
         if let user = self.authUser {
             //user logged in
+            debugPrint(user.email)
         } else {
             //user not logged in
             //present SignInVC
             presentSignInVC()
         }
-        
     }
     
+    @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            isSignedIn()
+        } catch let error as NSError  {
+            debugPrint(error.localizedDescription)
+        }
+    }
     
-    private func presentSignInVC(){
+    fileprivate func presentSignInVC(){
         let storyboard = UIStoryboard(name: Storyboard.AuthStoryboard, bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: StoryboardId.Auth)
-        present(controller, animated: false, completion: nil)
+        present(controller, animated: false, completion: nil) 
     }
 
 }
