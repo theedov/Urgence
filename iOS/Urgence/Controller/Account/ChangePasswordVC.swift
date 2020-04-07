c80ae88036ab9a9fdd3ab0ec437ddb5ac7edb3cc
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-04-07 11:58:04 +1000

    activity indicatior & current password textfield added

--- iOS/Urgence/Controller/Account/ChangePasswordVC.swift
+++ iOS/Urgence/Controller/Account/ChangePasswordVC.swift
@@ -12,6 +12,7 @@ class ChangePasswordVC: UIViewController {
     
     //Outlets
     @IBOutlet weak var passwordTxt: UTextField!
+    @IBOutlet weak var currentPasswordTxt: UTextField!
     @IBOutlet weak var confirmPasswordTxt: UTextField!
     @IBOutlet weak var passwordCheckImg: UIImageView!
     @IBOutlet weak var confirmPasswordCheckImg: UIImageView!
@@ -23,5 +24,16 @@ class ChangePasswordVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSavePressed(_ sender: Any) {
+        changePassword()
+    }
+    
+    func changePassword() {
+        self.authUser?.updatePassword(to: passwordTxt.text!, completion: { (error) in
+            if let _ = error {
+                AlertService.alert(state: .error, title: "Cannot change password", body: "There was an error changing your password. Please try again later or contact us directly: info@urgence.com.au", actionName: "I understand", vc: self, completion: nil)
+            }
+        })
+    }
+    
 }
