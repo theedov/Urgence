14d84940d36f669ceff6b8ef03ec2473026f3603
Bogdan Dovgopol <bogdan@dovgopol.dev>
2020-04-14 02:39:08 +1000

    Adding device flow updated

--- iOS/Urgence/Controller/Monitoring/QRDeviceScannerVC.swift
+++ iOS/Urgence/Controller/Monitoring/QRDeviceScannerVC.swift
@@ -8,37 +8,34 @@
 
 import UIKit
 import AVFoundation
+import Firebase
 
 class QRDeviceScannerVC: UIViewController {
-
+    
     //Outlets
     @IBOutlet weak var cameraPreviewView: UIView!
     
     //Variables
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
-    
-    //Delegate
-    var delegate: AddDeviceDelegate?
+    var deviceName: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         self.loadScanner()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         if (captureSession?.isRunning == false) {
             captureSession.startRunning()
         }
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         if (captureSession?.isRunning == true) {
             captureSession.stopRunning()
         }
@@ -47,7 +44,7 @@ class QRDeviceScannerVC: UIViewController {
     @IBAction func onBackPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-
+    
 }
 
 extension QRDeviceScannerVC: AVCaptureMetadataOutputObjectsDelegate {
@@ -55,35 +52,35 @@ extension QRDeviceScannerVC: AVCaptureMetadataOutputObjectsDelegate {
     func loadScanner(){
         view.backgroundColor = UIColor.black
         captureSession = AVCaptureSession()
-
+        
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
         let videoInput: AVCaptureDeviceInput
-
+        
         do {
             videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
             return
         }
-
+        
         if (captureSession.canAddInput(videoInput)) {
             captureSession.addInput(videoInput)
         } else {
             failed()
             return
         }
-
+        
         let metadataOutput = AVCaptureMetadataOutput()
-
+        
         if (captureSession.canAddOutput(metadataOutput)) {
             captureSession.addOutput(metadataOutput)
-
+            
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metadataOutput.metadataObjectTypes = [.qr]
         } else {
             failed()
             return
         }
-
+        
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = cameraPreviewView.layer.bounds
         previewLayer.videoGravity = .resizeAspectFill
@@ -95,26 +92,56 @@ extension QRDeviceScannerVC: AVCaptureMetadataOutputObjectsDelegate {
     
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
             AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
             found(code: stringValue)
         }
-
+        
         dismiss(animated: true)
     }
-
+    
     func found(code: String) {
-        delegate?.sendDeviceData(cameraId: code)
+        createFirestoreDevice(deviceId: code)
+    }
+    
+    func createFirestoreDevice(deviceId: String) {
+        let uid = authUser!.uid
+        let deviceRef = Firestore.firestore().collection("devices").whereField("deviceId", isEqualTo: deviceId)
+        
+        deviceRef.getDocuments { [weak self] (snap, error) in
+            if let error = error {
+                debugPrint("ERROR: \(error.localizedDescription)")
+                self?.dismiss(animated: true, completion: nil)
+                return
+            }
+            
+            if snap!.documents.isEmpty {
+                let data = ["room": self?.deviceName, "deviceId" : deviceId, "userId" : uid]
+                Firestore.firestore().collection("devices").addDocument(data: data as [String : Any]) { [weak self] (error) in
+                    if let error = error {
+                        debugPrint(error.localizedDescription)
+                        self?.dismiss(animated: true, completion: nil)
+                    }else {
+                        //Successfully added a device. Redirect back to MonitoringVC
+                        self?.navigationController?.popToRootViewController(animated: true)
+                    }
+                }
+            } else {
+                self!.dismiss(animated: true) {
+                    AlertService.alert(state: .error, title: "Cannot add a device", body: "This device id is already in use", actionName: "I understand", vc: self!, completion: nil)
+                }
+            }
+        }
     }
     
     func failed() {
-        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
         captureSession = nil
+        dismiss(animated: true) {
+            AlertService.alert(state: .error, title: "Scanning not supported", body: "Your device does not support scanning a code from an item. Please use a device with a camera.", actionName: "I understand", vc: self, completion: nil)
+        }
     }
     
 }
